017a16dcfd7f2e139366af3421ab42f5078eabac
heewon <littlekid916@naver.com>
2023-07-20 04:25:15 +0900

    front

--- front/src/views/Main/index.tsx
+++ front/src/views/Main/index.tsx
@@ -5,6 +5,8 @@ import { CurrentListResponseDto, Top3ListResponseDto } from 'src/interfaces/resp
 import { currentBoardListMock, popularWordListMock, top3ListMock } from 'src/mocks';
 import BoardListItem from 'src/components/BoardListItem';
 import { useNavigate } from 'react-router-dom';
+import { COUNT_BY_PAGE, COUNT_BY_SECTION, PAGE_BY_SECTION } from 'src/constants';
+import { getPagination } from 'src/utils';
 
 export default function Main() {
 
@@ -38,13 +40,64 @@ export default function Main() {
     const [currentList, setCurrentList] = useState<CurrentListResponseDto[]>([]);
     const [popularList, setPopularList] = useState<string[]>([]);
 
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const [currentSection, setCurrentSection] = useState<number>(1);
+    const [totalPage, setTotalPage] = useState<number[]>([]);
+    const [totalSection, setTotalSection] = useState<number>(1);
+
+    const [totalPageCount, setTotalPageCount] = useState<number>(0);
+    const [minPage, setMinPage] = useState<number>(0);
+    const [maxPage, setMaxPage] = useState<number>(0);
+
     const onPopularClickHandler = (word: string) => {
       navigator(`/search/${word}`);
     }
 
+    const onPageClickHandler = (page: number) => {
+      setCurrentPage(page);
+    }
+
+    const onPreviousClickHandler = () => {
+      // 한 페이지씩 이동
+      // if (currentPage != 1) setCurrentPage(currentPage - 1);
+
+      // 섹션 이동
+      // if (currentSection != 1) setCurrentSection(currentSection - 1);
+    
+      // 한 페이지씩 이동 + 섹션 이동
+      if (currentPage == 1) return;
+      if (currentPage == minPage) setCurrentSection(currentSection - 1);
+      setCurrentPage(currentPage - 1);
+    }
+
+    const onNextClickHandler = () => {
+      // 한 페이지씩 이동
+      // if (currentPage != totalPage.length) setCurrentPage(currentPage + 1);
+      
+      // 섹션 이동
+      // if (currentSection != totalSection) setCurrentSection(currentSection + 1);
+    
+      // 한 페이지씩 이동 + 섹션 이동
+      if (currentPage == totalPageCount) return;
+      if (currentPage == maxPage) setCurrentSection(currentSection + 1);
+      setCurrentPage(currentPage + 1);
+    }
+
     useEffect(() => {
+
+      const boardCount = 72;
+
+      const { section, maxPage, minPage, totalPageCount } = getPagination(boardCount, currentSection);
+      setTotalSection(section);
+      setMinPage(minPage);
+      setMaxPage(maxPage);
+      setTotalPageCount(totalPageCount);
+
       if (!currentList.length) setCurrentList(currentBoardListMock);
-    }, []);
+      const pageList = [];
+      for (let page = minPage; page <= maxPage; page++) pageList.push(page);
+      setTotalPage(pageList);
+    }, [currentSection]);
 
     useEffect(() => {
       if (!popularList.length) setPopularList(popularWordListMock);
@@ -66,7 +119,19 @@ export default function Main() {
             </div>
           </div>
         </div>
-        <div className='main-bottom-pagination'></div>
+        <div className='main-bottom-pagination'>
+          <div className='pagination-button' onClick={onPreviousClickHandler}>
+            <div className='pagination-left-icon'></div>
+            <div className='pagination-button-text'>이전</div>
+          </div>
+          <div className='pagination-text'>{'\|'}</div>
+          {totalPage.map((page) => (<div className={currentPage === page ? 'pagination-page-active' : 'pagination-page'} onClick={() => onPageClickHandler(page)}>{page}</div>))}
+          <div className='pagination-text'>{'\|'}</div>
+          <div className='pagination-button' onClick={onNextClickHandler}>
+            <div className='pagination-button-text'>다음</div>
+            <div className='pagination-right-icon'></div>
+          </div>
+        </div>
       </div>
     )
   }
